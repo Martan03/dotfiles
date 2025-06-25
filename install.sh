1ff94307ea6d27d5a6ee817abdef31143ed193c1
Martan03 <martinslezak03@gmail.com>
2025-06-25 23:21:08 +0200

    Add packages to install script

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 sudo pacman -Syu --needed base-devel clang composer discord dotnet-sdk ffmpeg \
 	firefox git glibc libc++ lldb llvm nodejs npm obs-studio php \
 	texlive texlive-langczechslovak ttf-cascadia-code vlc fd lazygit \
-	neovim zsh
+	neovim zsh jdk-openjdk
 
 # editing packages
 sudo pacman -S --needed gimp kdenlive
@@ -24,13 +24,13 @@ fi
 yay -S --needed makeit symfony-cli visual-studio-code-bin \
 	ttf-cascadia-code-nerd ttf-apple-emoji
 
+# Hyprland packages
+yay -S --needed hyprpaper grim slurp swappy cliphist mako nwg-look qt5ct \
+	breeze kvantum mako rofi-wayland waybar qt6ct-kde
+
 if ! type cargo &>/dev/null; then
 	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
-
-cp .zshrc ~/.zshrc
-cp .bashrc ~/.bashrc
-cp -r nvim ~/.config/nvim
 
 # Oh my zsh install
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -42,3 +42,7 @@ git clone https://github.com/zsh-users/zsh-autosuggestions \
 	${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 curl -L -o ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/typedark.zsh-theme \
 	https://raw.githubusercontent.com/BonnyAD9/TypeDark/master/zsh-typedark.sh
+
+cp .zshrc ~/.zshrc
+cp .bashrc ~/.bashrc
+cp -r nvim ~/.config/nvim
