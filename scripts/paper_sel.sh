ee4222e2644c1afeb2e813402c187b0fc77f12f9
Martan03 <martinslezak03@gmail.com>
2025-12-23 00:23:27 +0100

    Update paper_sel scripts to use packgs

--- scripts/paper_sel.sh
+++ scripts/paper_sel.sh
@@ -6,6 +6,8 @@
 # Usage:
 # `./paper_sel.sh [wallpaper_directory]`
 
+set -e
+
 THEME="$HOME/.config/rofi/themes/paper-picker.rasi"
 PAPER_DIR=${1:-/mnt/hdd/Images/Wallpapers}
 WAYBAR_RES="$HOME/.config/waybar/restart.sh"
@@ -15,50 +17,37 @@ TYPE="center"
 DURATION=2
 FPS=60
 
-monitor=$(hyprctl monitors -j | jq -r '.[] | select(.focused) | .name')
-mapfile -d '' PAPERS < <(find "$PAPER_DIR" -type f \( \
-    -iname "*.jpg" \
-    -o -iname "*.png" \
-    -o -iname "*.jpeg" \
-    -o -iname "*.webp" \
-\) -print0)
+mapfile -t PACKS < <(find "$PAPER_DIR" -mindepth 1 -maxdepth 1 -type d | sort)
 
-RND_PAPER="${PAPERS[$((RANDOM % ${#PAPERS[@]}))]}"
-RND_PAPER_NAME="¯\_('')_/¯"
+RND_PACK="${PACKS[$((RANDOM % ${#PACKS[@]}))]}"
+RND_PACK_NAME="¯\_('')_/¯ (rng)"
 
 run_rofi() {
     rofi -i -show -dmenu -config $THEME
 }
 
 menu() {
-    IFS=$'\n' sorted=($(sort <<<"${PAPERS[*]}"))
-    printf "%s\x00icon\x1f%s\n" "$RND_PAPER_NAME" "$RND_PAPER"
+    rnd_cover=$(find "$RND_PACK" -type f \( -iname "*.jpg" -o -iname "*.png" \
+        -o -iname "*.webp" -o -iname "*.gif" \) -print -quit)
+    printf "%s\x00icon\x1f%s\n" "$RND_PACK_NAME" "$rnd_cover"
 
-    for path in "${sorted[@]}"; do
+    for path in "${PACKS[@]}"; do
         name=$(basename "$path")
-        printf "%s\x00icon\x1f%s\n" "$(echo "$name" | cut -d. -f1)" "$path"
-    done
-}
-
-find_set_paper() {
-    local -n arr=$1
-    local needle=$2
-
-    for i in "${!arr[@]}"; do
-        path="${arr[$i]}"
-        filename=$(basename "$path")
-        if [[ "$filename" == "$needle"* ]]; then
-            set_paper "$path"
-            return 0
+        cover=$(find "$path" -type f \( -iname "*.jpg" -o -iname "*.png" \
+            -o -iname "*.webp" -o -iname "*.gif" \) -print -quit)
+        if [[ -z $cover ]]; then
+            continue
         fi
+        printf "%s\x00icon\x1f%s\n" "$(echo "$name" | cut -d. -f1)" "$cover"
     done
-    return 1
 }
 
 set_paper() {
-    echo "$1"
     params="--transition-fps $FPS --transition-duration $DURATION"
-    awww img "$1" -o "$monitor" --transition-type "$TYPE" $params
+    awww img "$1" -o "$2" --transition-type "$TYPE" $params
+}
+
+reload_colors() {
     wallust run $1
     $WAYBAR_RES
 }
@@ -73,7 +62,38 @@ if [[ -z "$choice" ]]; then
     exit 0;
 fi
 
-case "$choise" in
-$RND_PAPER_NAME) set_paper $RANDOM_PIC ;;
-*) find_set_paper PAPERS "$choice" ;;
-esac
+sel_pack=""
+if [[ "$choice" == "$RND_PACK_NAME" ]]; then
+    sel_pack=$RND_PACK
+else
+    sel_pack="$PAPER_DIR/$choice"
+fi
+
+mapfile -d '' PAPERS < <(find "$sel_pack" -type f \( \
+    -iname "*.jpg" \
+    -o -iname "*.png" \
+    -o -iname "*.jpeg" \
+    -o -iname "*.webp" \
+\) -print0)
+
+MONITORS=($(hyprctl monitors -j | jq -r '.[].name'))
+
+chosen=()
+len=${#PAPERS[@]}
+if [ $len -eq 1 ]; then
+    for _ in "${MONITORS[@]}"; do chosen+=("${PAPERS[0]}"); done
+else
+    mapfile -t SHUFFLED < <(printf "%s\n" "${PAPERS[@]}" | shuf)
+
+    for i in "${MONITORS[@]}"; do
+        chosen+=("${SHUFFLED[$((i % $len))]}")
+    done
+fi
+
+for i in "${!MONITORS[@]}"; do
+    set_paper "${chosen[$i]}" "${MONITORS[$i]}" &
+done
+
+wallust run "${chosen[0]}" &
+wait
+$WAYBAR_RES
